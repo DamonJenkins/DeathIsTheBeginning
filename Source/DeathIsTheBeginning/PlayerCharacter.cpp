2c25ae0e08906fd52a305924981c0c437f5b44db
Jedddie <34596339+Jedddie@users.noreply.github.com>
2020-02-18 21:30:55 +1300

    Added soul harvesting

--- Source/DeathIsTheBeginning/PlayerCharacter.cpp
+++ Source/DeathIsTheBeginning/PlayerCharacter.cpp
@@ -23,6 +23,20 @@ void APlayerCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	FHitResult result;
+
+
+	APlayerController* PlayerController = Cast<APlayerController>(GetController());
+	if (PlayerController != nullptr)
+	{
+		FHitResult TraceResult(ForceInit);
+		PlayerController->GetHitResultUnderCursor(ECollisionChannel::ECC_Visibility, false, TraceResult);
+		if (TraceResult.GetActor() != nullptr)
+		{
+			ACharacter * target;
+		}
+	}
+
 }
 
 // Called to bind functionality to input
